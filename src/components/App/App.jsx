1cac768f734af5d58665f5d58126c4539262b416
Vladyslav <vladyslavromanenkoo@gmail.com>
2023-07-03 14:19:05 +0300

    change hook useState

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,25 +4,33 @@ import { Form } from '../Form/Form';
 import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import css from './App.module.css';
+import contactsDefault from '../../data/contacts.json';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  // const [contacts, setContacts] = useState([
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ]);
+  const LOCAL_CONTACTS = 'local-contacts';
 
   const [filter, setFilter] = useState('');
 
-  const LOCAL_CONTACTS = 'local-contacts';
-
-  useEffect(() => {
-    const storedContacts = JSON.parse(localStorage.getItem(LOCAL_CONTACTS));
-    if (storedContacts) {
-      setContacts(storedContacts);
+  const [contacts, setContacts] = useState(() => {
+    const resultContacts = JSON.parse(localStorage.getItem(LOCAL_CONTACTS));
+    if (resultContacts) {
+      return resultContacts;
     }
-  }, []);
+    return contactsDefault;
+  });
+
+  // useEffect(() => {
+  //   const storedContacts = JSON.parse(localStorage.getItem(LOCAL_CONTACTS));
+  //   if (storedContacts) {
+  //     setContacts(storedContacts);
+  //   }
+  // }, []);
 
   useEffect(() => {
     localStorage.setItem(LOCAL_CONTACTS, JSON.stringify(contacts));
